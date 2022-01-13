67a9008c9eaca1127672e6fc9850ad7aff0e5f14
PhilipDavey <pjddavey@gmail.com>
2022-01-13 22:01:31 +0000

    added to pkg

--- AES/rtl/sub_bytes_rtl.vhd
+++ AES/rtl/sub_bytes_rtl.vhd
@@ -11,6 +11,27 @@ ENTITY sub_bytes IS
 
 ARCHITECTURE arch OF sub_bytes IS
 
+TYPE INT_OUTPUT_ARRAY IS ARRAY (0 TO 3) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL int_output : STD_LOGIC_VECTOR(127 DOWNTO 0);
+
 BEGIN
 
+    PROCESS(CLK, RST_N)
+    BEGIN
+        IF (RST_N = '0') THEN
+
+        ELSIF RISING_EDGE(CLK) THEN
+
+        FOR x IN 0 TO 3 LOOP
+            FOR i IN 0 TO LOOP
+                int_output <= 
+
+
+        END IF;
+    END PROCESS;
 END arch ;
+
+TO_INTEGER(UNSIGNED(INPUT_DATA(31 DOWNTO 24)));
+
+-- convert byte into integer, seperate into msb and lsb
+-- use that as coordinates for the table
